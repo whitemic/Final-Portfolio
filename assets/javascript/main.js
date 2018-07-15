1b2a0f148b88a3b8e6a5340385c4af6156c3db0e
Michaela White <whitemic@sas.upenn.edu>
2018-07-14 22:51:13 -0400

    Updated ted with parallax portfolio

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,20 +1,9 @@
-var index = 0;
+// /* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
-    var background1 = $('body').css('background-image', 'url("assets/images/img1.jpg")');
-    var background2 = $('body').css('background-image', 'url("assets/images/img2.jpg")');
-    var background3 = $('body').css('background-image', 'url("assets/images/img3.jpg")');
-    var background4 = $('body').css('background-image', 'url("assets/images/img4.jpg")');
-    // setTimeout(changeBackground, 5000);
-    // $("#wrapper").append(image);
-    $("body").fadeTo(3000, 0.7,changeBackground);
-
-
-function changeBackground() {
-    index++;
-    var bgImages = ['url("assets/images/img2.jpg")', 'url("assets/images/img3.jpg")', 'url("assets/images/img4.jpg")']
-    $('body').css('background-image', bgImages[index]);
-    if (index === 2) {
-        index = -1;
-    }
-
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
